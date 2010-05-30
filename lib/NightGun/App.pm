9149874f4f392a508d720197c772143ebe1a3a23
Eotect Nahn <eotect@gmail.com>
2010-05-30 11:56:13 +0000

    source clean up
    git-svn-id: https://nightgun.googlecode.com/svn/trunk@8 c2e8633a-0d85-b26b-fde5-0dbd65e1ee99

--- lib/NightGun/App.pm
+++ lib/NightGun/App.pm
@@ -1,28 +1,63 @@
 #!/usr/bin/perl -w
 package NightGun::App;
-BEGIN {
+use Exporter;
+use Term::ANSIColor;
+use Data::Dumper;
+my $VERSION = 1.15;
 use constant {
     NAME => 'NightGun',
-    VERSION => '1.0',
+    VERSION => '1.15',
     AUTHORS => 'xiaoranzzz@myplace.hell',
     COPYRIGHT => 'xiaoranzzz@myplace.hell 2008',
     COMMENTS => 'text reader suitable for night reading',
-    DATE => '2008-12-31',
+    DATE => '2010-05-18',
     GUI => 'GTK2',
-    VIEW => 'MozEmbed',
+#    VIEW => 'MozEmbed',
     LOGING => 0,
 #    VIEW => 'Normal',
-    DEBUG => 0,
+    DEBUG => 1,
 };
-    if(not NightGun::App::DEBUG) {
-        eval 'sub dump {};';
-    }
-    else {
-        eval '
-            use Data::Dumper;
-            sub dump  {foreach(@_){print STDERR (Dumper($_),"\n");}};
-        ';
-    }
+use constant {
+    WARN_COLOR=>color('yellow'),
+    MSG_COLOR=>color('green'),
+    ERR_COLOR=>color('red'),
+    NO_COLOR=>color('reset'),
+    HD_COLOR=>color('bold'),
+};
+
+our @ISA=qw/Exporter/;
+our @EXPORT=qw/&message &warn &error &say &dump/;
+sub _build_hd {
+    my $hd =shift;
+    $hd = $hd ? "NightGun::$hd>" : "NightGun>";
+}
+sub message {
+    no warnings;
+    my $hd=shift;$hd = _build_hd($hd);
+    print STDERR HD_COLOR,$hd,NO_COLOR,MSG_COLOR,@_,NO_COLOR,"\n";
+}
+sub warn {
+    no warnings;
+    my $hd=shift;$hd = _build_hd($hd);
+    print STDERR HD_COLOR,$hd,NO_COLOR,WARN_COLOR,@_,NO_COLOR,"\n";
+}
+sub error {
+    no warnings;
+    my $hd=shift;$hd = _build_hd($hd);
+    print STDERR HD_COLOR,$hd,ERR_COLOR,@_,NO_COLOR,"\n";
+}
+sub say {
+    my $hd=shift;$hd = _build_hd($hd);
+    print STDERR HD_COLOR,$hd,NO_COLOR,@_,NO_COLOR,"\n";
+}
+sub dump {
+    my $hd=shift;$hd = _build_hd($hd);
+    print STDERR HD_COLOR,$hd,NO_COLOR,Data::Dumper->Dump(@_),NO_COLOR,"\n";  
+}
+sub debug {
+    return unless(NightGun::App::DEBUG);
+    my $hd=shift;$hd = _build_hd($hd);
+    print STDERR HD_COLOR,$hd,ERR_COLOR,@_,NO_COLOR,"\n";
 }
 sub fill_about_dialog {
     my $about_window = shift;
